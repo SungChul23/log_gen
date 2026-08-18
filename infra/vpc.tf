b3aed30e73891ef3218861ebccfa40316be7fd59
SungChul23 <kimsam0923@naver.com>
2026-08-18 10:22:00 +0900

    feat(vpc): VPC 및 관련 리소스 구성 추가

---
+++ infra/vpc.tf
@@ -0,0 +1,62 @@
+# log 생성기 전용 VPC
+resource "aws_vpc" "this" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+
+  tags = {
+    Name = "${var.project_name}-vpc"
+  }
+}
+
+# igw
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.project_name}-igw"
+  }
+}
+
+# public subnet
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidrs)
+
+  vpc_id                  = aws_vpc.this.id
+  availability_zone       = local.availability_zones[count.index]
+  cidr_block              = var.public_subnet_cidrs[count.index]
+
+  # NAT 없이 인터넷 통신 가능하도록 퍼블릭 IP 할당
+  # NAT를 안쓰니 이 서브넷에 생성되는 리소스에 자동으로 퍼블릭 IP 할당
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.project_name}-public-${count.index + 1}"
+    Type = "loggen-public"
+  }
+}
+
+# rt
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.project_name}-public-rt"
+  }
+}
+
+# 외부 트래픽을 igw로 전달
+resource "aws_route" "internet" {
+  route_table_id = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.this.id
+}
+
+# 퍼블릭 서브넷 , igw 결합
+resource "aws_route_table_association" "public" {
+  count = length(aws_subnet.public)
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
