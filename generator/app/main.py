ac6f5894ca1859d9dc173dce33d3454aaa10a59c
SungChul23 <kimsam0923@naver.com>
2026-08-18 14:52:17 +0900

    feat(main): 로그 생성을 위한 faker 및 json 모듈 추가

--- generator/app/main.py
+++ generator/app/main.py
@@ -1,8 +1,16 @@
 # 엔트리 진입
+from app.domain import ecommerce
+from faker import Faker
+import json
+
+fake = Faker("ko_KR")
 
 def run():
-    
+    log = ecommerce.generate(fake, timezone_name="", envrionment="", run_id="")
     # 정상 실행 종료 표시
+    
+    print(json.dumps(log, ensure_ascii=False))
+    
     return 0
     
 
