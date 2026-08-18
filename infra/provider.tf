d863bd77400e898e286c59ce0d8e893178bb7dd2
SungChul23 <kimsam0923@naver.com>
2026-08-18 09:52:24 +0900

    feat(provider): AWS 프로바이더 및 가용영역 조회 추가

---
+++ infra/provider.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags ={
+        Project = var.project_name
+        ManageBy = "Terraform"
+        Purpose = "로그 재너레이터"
+    }
+  }
+}
+
+# 가용영역 조회
+data "aws_availability_zones" "available" {
+  state = "available"
+}
