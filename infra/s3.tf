5c6bbcaea055778548be29be3fe44f09df87e7a9
SungChul23 <kimsam0923@naver.com>
2026-08-20 13:26:22 +0900

    fix(infra): 잘못된 AWS 호출자 ID 문자열 수정

--- infra/s3.tf
+++ infra/s3.tf
@@ -3,7 +3,7 @@
 
 resource "aws_s3_bucket" "data" {
   # 버킷 1개 구성 -> 내부에 브론즈=>실버=>골드 레이어 관리
-  bucket = "${var.project_name}-s3-bk-${data.aws_caller_identity.current}"
+  bucket = "${var.project_name}-s3-bk-${data.aws_caller_identity.current.account_id}"
 
   # 버킷 삭제 될때 내부 데이터도 같이 삭제하게 할것인가 (강제)
   force_destroy = true
