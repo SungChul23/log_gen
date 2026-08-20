259342e4bfa64d14a111832f3a3b3037a6768bbf
SungChul23 <kimsam0923@naver.com>
2026-08-20 10:27:13 +0900

    feat(infra): Kinesis 스트림 리소스 추가

---
+++ infra/kinesis.tf
@@ -0,0 +1,10 @@
+resource "aws_kinesis_stream" "logs" {
+  name = local.kinesis_stream_name
+  shard_count = var.KDS_shard_count
+  retention_period = var.kinesis_retention_hour
+
+  # 구성 방식 - 프로비저닝으로 할당
+  stream_mode_details {
+    stream_mode = "PROVISIONED"
+  }
+}
